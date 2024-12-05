14ea9880c60c8046ee584bae38522fe102d5bda1
may-sec <165646909+may-sec@users.noreply.github.com>
2024-12-05 18:00:05 +0530

    Create id.py

---
+++ Linkedin Automated Messenging/id.py
@@ -0,0 +1,10 @@
+id = """
+volodymyr-yaroshyk-92380a238
+sian-burke-8b3063189
+meryem-arma%C4%9Fan-325620255
+viktoria-donskaya-18b16528b
+"""
+lines = id.splitlines()
+
+def get_id():
+    return lines
